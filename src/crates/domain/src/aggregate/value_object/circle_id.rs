54ed1e6df43496124b2c92956a0af585f8161be3
obaraelijah <elijahobara357@gmail.com>
2024-05-21 13:36:03 +0300

    feat: value object methods

--- src/crates/domain/src/aggregate/value_object/circle_id.rs
+++ src/crates/domain/src/aggregate/value_object/circle_id.rs
@@ -6,3 +6,28 @@ impl CircleId {
         Self(rand::random::<i16>())
     }
 }
+
+impl std::fmt::Display for CircleId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::hash::Hash for CircleId {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.hash(state)
+    }
+}
+
+impl std::convert::From<i16> for CircleId {
+    fn from(id: i16) -> Self {
+        Self(id)
+    }
+}
+
+impl std::convert::From<CircleId> for i16 {
+    fn from(circle_id: CircleId) -> Self {
+        circle_id.0 as i16
+    }
+}
+
