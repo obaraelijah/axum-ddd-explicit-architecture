b319e7348d478b0929aac5d1ebd2c3c5d4c92894
obaraelijah <elijahobara357@gmail.com>
2024-05-21 13:51:55 +0300

    feat: Domain intrefaces traits

---
+++ src/crates/domain/src/interface/circle_repository_interface.rs
@@ -0,0 +1,25 @@
+use anyhow::Error;
+
+use crate::aggregate::{circle::Circle, value_object::circle_id::CircleId};
+
+pub trait CircleRepositoryInterface {
+    fn find_by_id(
+        &self,
+        circle_id: &CircleId,
+    ) -> impl std::future::Future<Output = Result<Circle, Error>> + Send;
+
+    fn create(
+        &self,
+        circle: &Circle,
+    ) ->  impl std::future::Future<Output = Result<(), Error>> + Send;
+
+    fn update(
+        &self,
+        circle: &Circle,
+    ) -> impl std::future::Future<Output = Result<Circle, Error>> + Send;
+
+    fn delete(
+        &self,
+        circle: &Circle,
+    ) -> impl std::future::Future<Output = Result<(), Error>> + Send;
+}
