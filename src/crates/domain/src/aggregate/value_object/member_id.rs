8980ff0b9578a9c961b5f77f79c2c98c60648f22
obaraelijah <elijahobara357@gmail.com>
2024-05-21 13:37:39 +0300

    feat: value object member methods

--- src/crates/domain/src/aggregate/value_object/member_id.rs
+++ src/crates/domain/src/aggregate/value_object/member_id.rs
@@ -6,3 +6,27 @@ impl MemberId {
         Self(rand::random::<i16>())
     }
 }
+
+impl std::hash::Hash for MemberId {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+    }
+}
+
+impl std::fmt::Display for MemberId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::convert::From<i16> for MemberId {
+    fn from(id: i16) -> Self {
+        Self(id)
+    }
+}
+
+impl std::convert::From<MemberId> for i16 {
+    fn from(member_id: MemberId) -> Self {
+        member_id.0
+    }
+}
