20b752e45bed5896207922de11d7db1620d17a81
obaraelijah <elijahobara357@gmail.com>
2024-05-21 16:10:17 +0300

    feat: convertion from

--- src/handler.rs
+++ src/handler.rs
@@ -1,7 +1,62 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+};
+use usecase::create_circle::{CreateCircleInput, CreateCircleOutput};
 
 pub async fn handle_get_version() -> String {
     env!("CARGO_PKG_VERSION").to_string()
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct CreateCircleRequestBody {
+    pub circle_name: String,
+    pub capacity: i16,
+    pub owner_name: String,
+    pub owner_age: i16,
+    pub owner_grade: i16,
+    pub owner_major: String,
+}
+
+impl std::convert::From<CreateCircleRequestBody> for CreateCircleInput {
+    fn from(CreateCircleRequestBody {
+        circle_name,
+        capacity,
+        owner_name,
+        owner_age,
+        owner_grade,
+        owner_major,
+    }: CreateCircleRequestBody,
+    ) -> Self {
+        CreateCircleInput::new(
+            circle_name, 
+            capacity, 
+            owner_name, 
+            owner_age, 
+            owner_grade, 
+            owner_major
+        )
+    }
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+pub struct CreateCircleResponseBody {
+    pub circle_id: i16,
+    pub owner_id: i16,
+}
+
+impl std::convert::From<CreateCircleOutput> for CreateCircleResponseBody {
+    fn from(
+        CreateCircleOutput {
+            circle_id,
+            owner_id,
+        }: CreateCircleOutput,
+    ) -> Self {
+        CreateCircleResponseBody {
+            circle_id,
+            owner_id,
+        }
+    }
 }
 
 #[tracing::instrument(name = "handle_debug", skip())]
