bb907d4dfd55160c4155bfc05c7b49b8bd9bdda0
obaraelijah <elijahobara357@gmail.com>
2024-06-18 15:01:06 +0300

    fix: Fix id (#2)

--- src/handler.rs
+++ src/handler.rs
@@ -9,6 +9,7 @@ use sqlx::Row;
 use std::env;
 use usecase::{
     create_circle::{CreateCircleInput, CreateCircleOutput, CreateCircleUsecase},
+    fetch_all_circle::FetchAllCircleUsecase,
     fetch_circle::{FetchCircleInput, FetchCircleOutput, FetchCircleUsecase, MemberOutput},
     update_circle::{UpdateCircleInput, UpdateCircleOutPut, UpdateCircleUsecase},
 };
@@ -51,8 +52,8 @@ impl std::convert::From<CreateCircleRequestBody> for CreateCircleInput {
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct CreateCircleResponseBody {
-    pub circle_id: i16,
-    pub owner_id: i16,
+    pub circle_id: String,
+    pub owner_id: String,
 }
 
 impl std::convert::From<CreateCircleOutput> for CreateCircleResponseBody {
@@ -85,12 +86,12 @@ pub async fn handle_create_circle(
 
 #[derive(Debug, Deserialize)]
 pub struct FetchCircleInputParam {
-    id: i16,
+    id: String,
 }
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
 pub struct FetcheCircleResponseBody {
-    pub circle_id: i16,
+    pub circle_id: String,
     pub circle_name: String,
     pub capacity: i16,
     pub owner: MemberOutput,
@@ -131,9 +132,16 @@ pub async fn handle_fetch_circle(
         .map_err(|e| e.to_string())
 }
 
+pub async fn handle_fetch_all(State(state): State<AppState>) -> impl IntoResponse {
+    let usecase = FetchAllCircleUsecase::new(state.circle_repository);
+    let circles = usecase.execute().await;
+    tracing::info!("circles: {:?}", circles);
+    (StatusCode::OK).into_response()
+}
+
 #[derive(Debug, Deserialize)]
 pub struct UpdateCircleInputParam {
-    id: i16,
+    id: String,
 }
 
 #[derive(Debug, serde::Deserialize, serde::Serialize)]
@@ -143,19 +151,19 @@ pub struct UpdateCircleRequestBody {
 }
 
 impl UpdateCircleRequestBody {
-    pub fn convert_to_input(self, id: i16) -> UpdateCircleInput {
+    pub fn convert_to_input(self, id: String) -> UpdateCircleInput {
         UpdateCircleInput::new(id, self.circle_name, self.capacity)
     }
 }
 
 #[derive(Debug, serde::Serialize)]
 pub struct UpdateCircleResponseBody {
-    pub id: i16,
+    pub circle_id: String,
 }
 
 impl std::convert::From<UpdateCircleOutPut> for UpdateCircleResponseBody {
-    fn from(UpdateCircleOutPut { id }: UpdateCircleOutPut) -> Self {
-        UpdateCircleResponseBody { id }
+    fn from(UpdateCircleOutPut { circle_id }: UpdateCircleOutPut) -> Self {
+        UpdateCircleResponseBody { circle_id }
     }
 }
 
@@ -164,7 +172,7 @@ pub async fn handle_update_circle(
     Path(path): Path<UpdateCircleInputParam>,
     Json(body): Json<UpdateCircleRequestBody>,
 ) -> Result<Json<UpdateCircleResponseBody>, String> {
-    let update_circle_input = body.convert_to_input(path.id);
+    let update_circle_input = body.convert_to_input(path.id.to_string());
     let mut usecase = UpdateCircleUsecase::new(state.circle_repository);
 
     usecase
@@ -214,7 +222,6 @@ pub async fn handle_get_test(State(state): State<AppState>) -> impl IntoResponse
         }
     };
 
-    // log info
     for row in member_rows {
         let name = row.get::<String, _>("name");
         tracing::info!("member: {:?}", name);
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::{
     config::connect::connect,
-    handler::{handle_create_circle, handle_fetch_circle, handle_update_circle},
+    handler::{handle_create_circle, handle_fetch_all, handle_fetch_circle, handle_update_circle},
 };
 
 use axum::{
@@ -12,7 +12,6 @@ use infrastructure::circle_repository_with_my_sql::CircleRepositoryWithMySql;
 
 mod config;
 mod handler;
-mod tests;
 
 #[derive(Clone)]
 struct AppState {
@@ -23,9 +22,10 @@ struct AppState {
 fn router() -> Router<AppState> {
     Router::new()
         .route("/", get(handle_get_version))
-        .route("/circle:id", get(handle_fetch_circle))
-        .route("/circle/:id", put(handle_update_circle))
+        .route("/circle/:id", get(handle_fetch_circle))
+        .route("/circle", get(handle_fetch_all))
         .route("/circle", post(handle_create_circle))
+        .route("/circle/:id", put(handle_update_circle))
         .route("/test", get(handle_get_test))
         .route("/debug", get(handle_debug))
 }
@@ -35,7 +35,6 @@ async fn main() -> Result<(), ()> {
     tracing_subscriber::fmt().init();
 
     let pool = connect().await.expect("database should connect");
-
     let state = AppState {
         circle_repository: CircleRepositoryWithMySql::new(pool.clone()),
         pool,
@@ -51,3 +50,225 @@ async fn main() -> Result<(), ()> {
     Ok(())
 }
 
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::{
+        config::connect::connect_test,
+        handler::{CreateCircleRequestBody, CreateCircleResponseBody, UpdateCircleRequestBody},
+    };
+    use axum::http::{header::CONTENT_TYPE, StatusCode};
+    use domain::{
+        aggregate::{
+            circle::Circle,
+            member::Member,
+            value_object::{circle_id::CircleId, grade::Grade, major::Major, member_id::MemberId},
+        },
+        interface::circle_repository_interface::CircleRepositoryInterface,
+    };
+    use tower::ServiceExt;
+
+    use super::*;
+
+    // FIXME: ignore test because it requires a running database
+    #[tokio::test]
+    #[ignore]
+    async fn test_version() -> anyhow::Result<()> {
+        let pool = connect_test().await.expect("database should connect");
+        let state = AppState {
+            circle_repository: CircleRepositoryWithMySql::new(pool.clone()),
+            pool,
+        };
+        let app = router().with_state(state);
+        let response = app
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("GET")
+                    .uri("/")
+                    .body(axum::body::Body::empty())?,
+            )
+            .await?;
+        assert_eq!(response.status(), StatusCode::OK);
+        let response_body = String::from_utf8(
+            axum::body::to_bytes(response.into_body(), usize::MAX)
+                .await?
+                .to_vec(),
+        )?;
+        assert_eq!(response_body, "0.1.0");
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_create_circle() -> anyhow::Result<()> {
+        let pool = connect_test().await.expect("database should connect");
+        let state = AppState {
+            circle_repository: CircleRepositoryWithMySql::new(pool.clone()),
+            pool,
+        };
+        let app = router().with_state(state.clone());
+        let response = app
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("POST")
+                    .uri("/circle")
+                    .header(CONTENT_TYPE, "application/json")
+                    .body(axum::body::Body::new(serde_json::to_string(
+                        &CreateCircleRequestBody {
+                            circle_name: "circle_name1".to_string(),
+                            capacity: 10,
+                            owner_name: "owner1".to_string(),
+                            owner_age: 21,
+                            owner_grade: 3,
+                            owner_major: "Music".to_string(),
+                        },
+                    )?))?,
+            )
+            .await?;
+        assert_eq!(response.status(), StatusCode::OK);
+        let response_body = serde_json::from_slice::<'_, CreateCircleResponseBody>(
+            &axum::body::to_bytes(response.into_body(), usize::MAX).await?,
+        )?;
+
+        let created = state
+            .circle_repository
+            .find_by_id(&CircleId::from_str(&response_body.circle_id)?)
+            .await?;
+        let circle = Circle::reconstruct(
+            CircleId::from_str(&response_body.circle_id)?,
+            "circle_name1".to_string(),
+            Member::reconstruct(
+                MemberId::from_str(&response_body.owner_id)?,
+                "owner1".to_string(),
+                21,
+                Grade::try_from(3)?,
+                Major::Music,
+            ),
+            10,
+            vec![],
+        );
+        assert_eq!(created, circle);
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_fetch_circle() -> anyhow::Result<()> {
+        let pool = connect_test().await.expect("database should connect");
+        let state = AppState {
+            circle_repository: CircleRepositoryWithMySql::new(pool.clone()),
+            pool,
+        };
+        let app = router().with_state(state);
+        let unexist_circle_id = 0;
+        let response = app
+            .clone()
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("GET")
+                    .uri(format!("/circle/{}", unexist_circle_id))
+                    .body(axum::body::Body::empty())?,
+            )
+            .await?;
+        assert_eq!(response.status(), StatusCode::OK);
+        let response_body = String::from_utf8(
+            axum::body::to_bytes(response.into_body(), usize::MAX)
+                .await?
+                .to_vec(),
+        )?;
+        assert_eq!(response_body, "Circle not found");
+
+        let (circle_id, owner_id) = build_circle(&app).await?;
+
+        let fetched_response = app
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("GET")
+                    .uri(format!("/circle/{}", circle_id))
+                    .body(axum::body::Body::empty())?,
+            )
+            .await?;
+        assert_eq!(fetched_response.status(), StatusCode::OK);
+        let fetched_response_body = String::from_utf8(
+            axum::body::to_bytes(fetched_response.into_body(), usize::MAX)
+                .await?
+                .to_vec(),
+        )?;
+        assert_eq!(
+            fetched_response_body,
+            format!(
+                "{{\"circle_id\":{},\"circle_name\":\"Music club\",\"capacity\":10,\"owner\":{{\"id\":{},\"name\":\"John Lennon\",\"age\":21,\"grade\":3,\"major\":\"Music\"}},\"members\":[]}}",
+                circle_id,owner_id
+            )
+        );
+        Ok(())
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn test_update_circle() -> anyhow::Result<()> {
+        let pool = connect_test().await.expect("database should connect");
+        let state = AppState {
+            circle_repository: CircleRepositoryWithMySql::new(pool.clone()),
+            pool,
+        };
+        let app = router().with_state(state.clone());
+        let (circle_id, _) = build_circle(&app).await?;
+        let update_response = app
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("PUT")
+                    .uri(format!("/circle/{}", circle_id))
+                    .header(CONTENT_TYPE, "application/json")
+                    .body(axum::body::Body::new(serde_json::to_string(
+                        &UpdateCircleRequestBody {
+                            circle_name: Some("Football club".to_string()),
+                            capacity: Some(20),
+                        },
+                    )?))?,
+            )
+            .await?;
+        assert_eq!(update_response.status(), StatusCode::OK);
+
+        let updated_circle = state
+            .circle_repository
+            .find_by_id(&CircleId::from_str(&circle_id)?)
+            .await?;
+        assert_eq!(updated_circle.name, "Football club");
+        assert_eq!(updated_circle.capacity, 20);
+
+        Ok(())
+    }
+
+    async fn build_circle(app: &Router) -> anyhow::Result<(String, String)> {
+        let create_response = app
+            .clone()
+            .oneshot(
+                axum::http::Request::builder()
+                    .method("POST")
+                    .uri("/circle")
+                    .header(CONTENT_TYPE, "application/json")
+                    .body(axum::body::Body::new(serde_json::to_string(
+                        &CreateCircleRequestBody {
+                            circle_name: "Music club".to_string(),
+                            capacity: 10,
+                            owner_name: "John Lennon".to_string(),
+                            owner_age: 21,
+                            owner_grade: 3,
+                            owner_major: "Music".to_string(),
+                        },
+                    )?))?,
+            )
+            .await?;
+        assert_eq!(create_response.status(), StatusCode::OK);
+        let create_response_body = serde_json::from_slice::<CreateCircleResponseBody>(
+            &axum::body::to_bytes(create_response.into_body(), usize::MAX).await?,
+        )?;
+
+        Ok((
+            create_response_body.circle_id,
+            create_response_body.owner_id,
+        ))
+    }
+}
